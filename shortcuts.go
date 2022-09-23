00ab214f66f90b13f61c364383e95e00a3a88bac
Tim Dévé <TimDeve@users.noreply.github.com>
2022-09-23 15:19:05 +0100

    Adds more fields to Shortcut
    The following fields have been added:
      AppId    uint32
      AppName  string
      Exe      string
      Icon     string
      IsHidden bool
      StartDir string
      Tags     []string

--- shortcuts.go
+++ shortcuts.go
@@ -7,8 +7,13 @@ import (
 )
 
 type Shortcut struct {
-	AppName string
-	Exe     string
+	AppId    uint32
+	AppName  string
+	Exe      string
+	Icon     string
+	IsHidden bool // Doesn't seem to be used by steam anymore
+	StartDir string
+	Tags     []string
 }
 
 func ParseShortcuts(buf io.Reader) ([]Shortcut, error) {
@@ -31,6 +36,11 @@ func ParseShortcuts(buf io.Reader) ([]Shortcut, error) {
 			return []Shortcut{}, errors.New("vdf that should be an array does not have the corresponding index")
 		}
 
+		appId, ok := s.GetUint("appid")
+		if !ok {
+			return []Shortcut{}, errors.New("Could not get key 'appid' for one of the shortcuts")
+		}
+
 		appName, ok := s.GetString("AppName")
 		if !ok {
 			return []Shortcut{}, errors.New("Could not get key 'AppName' for one of the shortcuts")
@@ -41,9 +51,50 @@ func ParseShortcuts(buf io.Reader) ([]Shortcut, error) {
 			return []Shortcut{}, errors.New("Could not get key 'Exe' for one of the shortcuts")
 		}
 
+		startDir, ok := s.GetString("StartDir")
+		if !ok {
+			return []Shortcut{}, errors.New("Could not get key 'StartDir' for one of the shortcuts")
+		}
+
+		icon, ok := s.GetString("icon")
+		if !ok {
+			return []Shortcut{}, errors.New("Could not get key 'icon' for one of the shortcuts")
+		}
+
+		isHidden, ok := s.GetBool("IsHidden")
+		if !ok {
+			return []Shortcut{}, errors.New("Could not get key 'IsHidden' for one of the shortcuts")
+		}
+
+		tagsMap, ok := s.GetMap("tags")
+		if !ok {
+			return []Shortcut{}, errors.New("Could not get key 'tags' for one of the shortcuts")
+		}
+
+		tags := []string{}
+		for i := 0; i < len(tagsMap); i++ {
+			key := strconv.Itoa(i)
+			t, ok := tagsMap[key]
+			if !ok {
+				return []Shortcut{}, errors.New("vdf that should be an array does not have the corresponding index")
+			}
+
+			ts, ok := t.AsString()
+			if !ok {
+				return []Shortcut{}, errors.New("tag should be a string but wasn't")
+			}
+
+			tags = append(tags, ts)
+		}
+
 		shortcuts[i] = Shortcut{
-			AppName: appName,
-			Exe:     exe,
+			AppId:    appId,
+			AppName:  appName,
+			Exe:      exe,
+			Icon:     icon,
+			IsHidden: isHidden,
+			StartDir: startDir,
+			Tags:     tags,
 		}
 	}
 
--- shortcuts_test.go
+++ shortcuts_test.go
@@ -18,9 +18,33 @@ func TestParseShortcut(t *testing.T) {
 	test.Nil(t, err)
 
 	expected := []vdf.Shortcut{
-		{AppName: "Control", Exe: "\"C:\\Program Files\\Epic Games\\Control\\Control_DX12.exe\""},
-		{AppName: "Cyberpunk 2077", Exe: "\"C:\\Program Files (x86)\\GOG Galaxy\\GalaxyClient.exe\""},
-		{AppName: "Skate 3", Exe: "\"C:\\Users\\user\\scoop\\apps\\RPCS3\\current\\rpcs3.exe\""},
+		{
+			AppId:    3414143657,
+			AppName:  "Control",
+			Exe:      "\"C:\\Program Files\\Epic Games\\Control\\Control_DX12.exe\"",
+			Icon:     "",
+			IsHidden: true,
+			StartDir: "\"C:\\Program Files\\Epic Games\\Control\\\"",
+			Tags:     []string{},
+		},
+		{
+			AppId:    3022575626,
+			AppName:  "Cyberpunk 2077",
+			Exe:      "\"C:\\Program Files (x86)\\GOG Galaxy\\GalaxyClient.exe\"",
+			Icon:     "C:\\Users\\user\\Downloads\\cyberpunk.ico",
+			IsHidden: false,
+			StartDir: "\"C:\\Program Files (x86)\\GOG Galaxy\\\"",
+			Tags:     []string{"favorite"},
+		},
+		{
+			AppId:    3043193801,
+			AppName:  "Skate 3",
+			Exe:      "\"C:\\Users\\user\\scoop\\apps\\RPCS3\\current\\rpcs3.exe\"",
+			Icon:     "",
+			IsHidden: false,
+			StartDir: "\"C:\\Users\\user\\scoop\\apps\\RPCS3\\current\\\"",
+			Tags:     []string{"Sport", "Action", "Skate"},
+		},
 	}
 
 	test.Equal(t, expected, shortcuts)
